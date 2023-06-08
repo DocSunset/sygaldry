ba5e0335077e40f8570f26dbb2993a68ac5fcef6
Travis West <travis@traviswest.ca>
2023-06-08 10:19:13 -0400

    one-bit-bongo: reorg, set up new main firmware dir

---
+++ instruments/one-bit-bongo/one-bit-bongo-esp32/main/one-bit-bongo.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+extern "C" void app_main(void)
+{
+    printf("Hello world.\n");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
