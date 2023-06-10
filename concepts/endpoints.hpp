901c470bf4ea60c3214f1a2f7ed925f8511b4ed3
Travis West <travis@traviswest.ca>
2023-06-10 11:10:04 -0400

    concepts/endpoints: refactor static asserts in value_of
    for compilers that still check asertions in constexpr branches that are
    unreachable..

--- concepts/endpoints.hpp
+++ concepts/endpoints.hpp
@@ -5,109 +5,111 @@
 
 namespace sygaldry { namespace concepts {
 
-    template<typename T>
-    concept has_range = requires
+template<typename T>
+concept has_range = requires
+{
+    std::decay_t<T>::range().min;
+    std::decay_t<T>::range().max;
+    std::decay_t<T>::range().init;
+};
+
+template<has_range T>
+constexpr auto get_range(const T&) { return T::range(); }
+
+template<has_range T>
+_consteval auto get_range() { return std::decay_t<T>::range(); }
+template <typename T, typename Y>
+concept strict_similar_to 
+    =  requires (T t) { static_cast<Y>(t); }
+    && requires (Y y) { T{y}; }
+    && requires (T t, Y y) { t = y; }
+    && requires (T a, T b, Y c) { a = b = c; }
+    ;
+
+template<typename T, typename Y>
+concept similar_to = strict_similar_to<std::remove_cvref_t<T>, std::remove_cvref_t<Y>>;
+template <typename T>
+concept has_value_variable = requires (T t)
+{
+    t.value;
+    requires similar_to<T, decltype(t.value)>;
+};
+
+template <typename T>
+concept has_value_method = requires (T t)
+{
+    t.value();
+    requires similar_to<T, decltype(t.value())>;
+};
+
+template <typename T>
+concept PersistentValue
+    =  has_value_variable<T> || has_value_method<T>
+    && std::default_initializable<std::remove_cvref_t<T>>;
+template<typename T>
+concept Flag = requires (T t)
+{
+    bool(t);
+    requires std::is_same_v<std::integral_constant<bool, bool(T{})>, std::false_type>;
+    requires std::is_same_v<std::integral_constant<bool, bool(T{T{}})>, std::false_type>;
+    t = T{};
+};
+
+template<typename T>
+concept _occasional_value = requires (T t)
+{
+    *t;
+    T{*t};
+    *t = *t;
+} && Flag<T>;
+
+template<typename T>
+concept OccasionalValue = _occasional_value<T> || _occasional_value<std::decay_t<T>>;
+template<typename T>
+concept Bang = requires (T t)
+{
+    requires std::is_enum_v<decltype(T::bang)>;
+} && Flag<T>;
+template<typename T>
+concept ClearableFlag = Flag<T> && (OccasionalValue<T> || Bang<T>);
+
+template<ClearableFlag T>
+constexpr void clear_flag(T& t)
+{
+    t = T{};
+}
+template <typename T>
+concept has_value = OccasionalValue<T> || PersistentValue<T>;
+
+template <has_value T>
+auto& value_of(T& v)
+{
+    static_assert(PersistentValue<T> || OccasionalValue<T>, "value_of: Neither PersistentValue nor OccasionalValue. Did we add a new kind?");
+    if constexpr (PersistentValue<T>)
     {
-        std::decay_t<T>::range().min;
-        std::decay_t<T>::range().max;
-        std::decay_t<T>::range().init;
-    };
-
-    template<has_range T>
-    constexpr auto get_range(const T&) { return T::range(); }
-
-    template<has_range T>
-    _consteval auto get_range() { return std::decay_t<T>::range(); }
-    template <typename T, typename Y>
-    concept strict_similar_to 
-        =  requires (T t) { static_cast<Y>(t); }
-        && requires (Y y) { T{y}; }
-        && requires (T t, Y y) { t = y; }
-        && requires (T a, T b, Y c) { a = b = c; }
-        ;
-
-    template<typename T, typename Y>
-    concept similar_to = strict_similar_to<std::remove_cvref_t<T>, std::remove_cvref_t<Y>>;
-    template <typename T>
-    concept has_value_variable = requires (T t)
-    {
-        t.value;
-        requires similar_to<T, decltype(t.value)>;
-    };
-
-    template <typename T>
-    concept has_value_method = requires (T t)
-    {
-        t.value();
-        requires similar_to<T, decltype(t.value())>;
-    };
-
-    template <typename T>
-    concept PersistentValue
-        =  has_value_variable<T> || has_value_method<T>
-        && std::default_initializable<std::remove_cvref_t<T>>;
-    template<typename T>
-    concept Flag = requires (T t)
-    {
-        bool(t);
-        requires std::is_same_v<std::integral_constant<bool, bool(T{})>, std::false_type>;
-        requires std::is_same_v<std::integral_constant<bool, bool(T{T{}})>, std::false_type>;
-        t = T{};
-    };
-
-    template<typename T>
-    concept _occasional_value = requires (T t)
-    {
-        *t;
-        T{*t};
-        *t = *t;
-    } && Flag<T>;
-
-    template<typename T>
-    concept OccasionalValue = _occasional_value<T> || _occasional_value<std::decay_t<T>>;
-    template<typename T>
-    concept Bang = requires (T t)
-    {
-        requires std::is_enum_v<decltype(T::bang)>;
-    } && Flag<T>;
-    template<typename T>
-    concept ClearableFlag = Flag<T> && (OccasionalValue<T> || Bang<T>);
-
-    template<ClearableFlag T>
-    constexpr void clear_flag(T& t)
-    {
-        t = T{};
+        static_assert(has_value_variable<T> || has_value_method<T>, "value_of: PersistentValue with no method or variable. Did we add a new kind?");
+        if constexpr (has_value_variable<T>) return v.value;
+        else if constexpr (has_value_method<T>) return v.value();
+        else return 0;
     }
-    template <typename T>
-    concept has_value = OccasionalValue<T> || PersistentValue<T>;
-
-    template <has_value T>
-    auto& value_of(T& v)
+    else if constexpr (OccasionalValue<T>)
     {
-        if constexpr (PersistentValue<T>)
-        {
-            if constexpr (has_value_variable<T>) return v.value;
-            else if constexpr (has_value_method<T>) return v.value;
-            else static_assert(false, "value_of: PersistentValue with no method or variable. Did we add a new kind?");
-        }
-        else if constexpr (OccasionalValue<T>)
-        {
-            return *v;
-        }
-        else static_assert(false, "value_of: Neither PersistentValue nor OccasionalValue. Did we add a new kind?");
+        return *v;
     }
+    else return 0;
+}
 
-    template<has_value T>
-    const auto& value_of(const T& v)
-    {
-        return value_of(const_cast<T&>(v));
-    }
+template<has_value T>
+const auto& value_of(const T& v)
+{
+    return value_of(const_cast<T&>(v));
+}
 
-    template <has_value T>
-    auto& set_value(T& v, const auto& arg)
-    {
-        v = arg;
-        return v;
-    }
+template <has_value T>
+auto& set_value(T& v, const auto& arg)
+{
+    v = arg;
+    return v;
+}
 
 } } // namespaces
