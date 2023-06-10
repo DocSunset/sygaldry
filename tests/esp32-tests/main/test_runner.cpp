e766067bcc9fdead2ff19934f2f67695c1565b54
Travis West <travis@traviswest.ca>
2023-06-10 13:44:15 -0400

    esp32-tests: wait before running

--- tests/esp32-tests/main/test_runner.cpp
+++ tests/esp32-tests/main/test_runner.cpp
@@ -1,9 +1,18 @@
-#include "unity.h"
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <unity.h>
 #include <components/esp32/tests/gpio.hpp>
 #include <components/esp32/tests/button.hpp>
 
 extern "C" void app_main(void)
 {
+    printf("Type g to run tests.\n");
+
+    while ('g' != getchar()) vTaskDelay(50 / portTICK_PERIOD_MS);
+
+    vTaskDelay(100 / portTICK_PERIOD_MS); // give time for test host to connect
+
     UNITY_BEGIN();
     RUN_TEST(gpio);
     RUN_TEST(button);
