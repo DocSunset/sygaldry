40bda355ab49fbaed4b5498374fbe76c48ce5b7b
Travis West <travis@traviswest.ca>
2023-06-10 09:18:58 -0400

    build: don't try to regenerate litlit file with no tangles

--- sh/litlit.sh
+++ sh/litlit.sh
@@ -2,7 +2,7 @@
 find -iname '*.litlit' | parallel '
     cd {//}
     generated="$(grep -m 1 -h -r -I "@#" {/} | head -n 1 | sed 's/^.*@#.//' | sed 's/.$//')"
-    [ {/} -nt "$generated" ] &&
+    [ "$generated" ] && [ {/} -nt "$generated" ] &&
     {
         echo {} "newer than $generated, running litlit..."
         litlit {/}
