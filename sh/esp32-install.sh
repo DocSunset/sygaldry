d34d54acf51c1782b2270403fa86f583cc3f68db
Travis West <travis@traviswest.ca>
2023-07-24 14:40:22 -0400

    remove lingering unused esp32-install.sh

--- sh/esp32-install.sh
+++
@@ -1,5 +0,0 @@
-echo $0
-[ "$1" = "$ESPPROJ" ] || { echo "Skipping this build ($1) which doesn't match ESPPROJ ($ESPPROJ)" ; exit ; }
-shift 1
-[ -c "$ESPPORT" -a -w "$ESPPORT" ] && $@ || echo "No valid ESPPORT ($ESPPORT), skipping install"
-echo "flashed successfully"
